f3ad558d05bd535262643f07246b071d35509957
sinafarahani <sfarahani15@gmail.com>
2019-02-14 18:01:50 +0330

    add user done

--- delta/delta/settings.py
+++ delta/delta/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 AUTH_USER_MODEL = 'dashboard.User'
 
